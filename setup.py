82a3a97a10cfe2a03763829c82c57fec9bc84a23
Kenneth M. Elf Sternberg <elf.sternberg@gmail.com>
2016-10-05 10:53:57 -0700

    Bump version: 0.0.4 → 0.0.4

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ test_requirements = [
 
 setup(
     name='git_linter',
-    version='0.0.4',
+    version='version='0.0.4'',
     description="A git command to lint everything in your workspace (or stage) that was changed since the last commit.",
     long_description=readme + '\n\n' + history,
     author='Kenneth M. "Elf" Sternberg',
